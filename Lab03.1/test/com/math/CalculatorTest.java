99dcd0ae30321003ceb49eccf40c961e6eb2625e
SSPENELOPE <typoepping@gmail.com>
2023-11-02 11:28:27 -0400

    Implemented Unit Testing

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,33 @@
 package com.math;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+    private Calculator calc;
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    @Before
+    public void init() {
+        calc = new Calculator();
+    }
+
+
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(5, calc.divide(10,2), .001);
+    }
+
+    @Test
+    public void testIsEven() {
+        assertTrue(calc.isEven(10));
+        assertFalse(calc.isEven(5));
+    }
 }
