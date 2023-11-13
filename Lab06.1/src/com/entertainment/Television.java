38002773012a1122492aa8000293429dc144a361
SSPENELOPE <typoepping@gmail.com>
2023-11-13 11:11:23 -0500

    Lab05.3 Update

--- Lab06.1/src/com/entertainment/Television.java
+++ Lab06.1/src/com/entertainment/Television.java
@@ -119,4 +119,16 @@ implements Comparable<Television> {
     }
     return result;
   }
+
+
+  class Tuner {
+    private int channel = 3;  // default channel for cable and satellite customers
+
+    public int getChannel() {
+      return this.channel;
+    }
+    public void setChannel(int channel) {
+      this.channel = channel;
+    }
+  }
 }
